ba93a72cdceb949bcc01cf92b9c6a2853c681f21
mutasem <motasemimad10@gmail.com>
2021-08-24 15:21:11 +0300

    rendering dummy post data

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -6,7 +6,7 @@ function PostsGrid(props) {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem />
+        <PostItem post={post} key={post.slug} />
       ))}
     </ul>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import { Fragment } from 'react';
+import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
 
 function HomePage() {
+  const DUMMY_POSTS = [
+    {
+      slug: 'getting-started-with-nextjs',
+      title: 'Getting started with Next JS',
+      image: 'getting-started-nextjs.png',
+      excerpt:
+        'Next Js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+      date: '2022-2-10',
+    },
+    {
+      slug: 'getting-started-with-nextjs2',
+      title: 'Getting started with Next JS',
+      image: 'getting-started-nextjs.png',
+      excerpt:
+        'Next Js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+      date: '2022-2-10',
+    },
+    {
+      slug: 'getting-started-with-nextjs3',
+      title: 'Getting started with Next JS',
+      image: 'getting-started-nextjs.png',
+      excerpt:
+        'Next Js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+      date: '2022-2-10',
+    },
+    {
+      slug: 'getting-started-with-nextjs4',
+      title: 'Getting started with Next JS',
+      image: 'getting-started-nextjs.png',
+      excerpt:
+        'Next Js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+      date: '2022-2-10',
+    },
+  ];
+
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
