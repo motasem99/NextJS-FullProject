c894fd850a91bcfe8f379bebc8d47d9439c02d91
mutasem <motasemimad10@gmail.com>
2021-08-24 14:10:44 +0300

    adding the hero components

---
+++ components/home-page/hero.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import classes from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/mutasem.jpeg'
+          alt='An image showing mutasem'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi , I'm Mutasem</h1>
+      <p>
+        Frontend Web Developer with 2+ experience in React JS, with problem
+        solving skills and team working.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { Fragment } from 'react';
+import Hero from '../components/home-page/hero';
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
     </Fragment>
   );
 }
