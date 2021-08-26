ecc4500d54d1920cf3b7a7c896870b969e65d3b9
mutasem <motasemimad10@gmail.com>
2021-08-26 17:23:34 +0300

    rendering images with the next image component from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -6,10 +7,54 @@ function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  // const customRenderers = {
+  //   image(image) {
+  //     return (
+  //       <Image
+  //         src={`/images/posts/${post.slug}/${image.src}`}
+  //         alt={image.alt}
+  //         width={600}
+  //         height={300}
+  //       />
+  //     );
+  //   },
+  //   paragraph(paragraph) {
+  //     const { node } = paragraph;
+  //     if (node.children[0].type === 'image') {
+  //       const image = node.children[0];
+  //       console.log(image);
+  //       return (
+  //         <div className={classes.image}>
+  //           <Image
+  //             src={`/images/posts/${post.slug}/${image.url}`}
+  //             alt={image.alt}
+  //             width={600}
+  //             height={300}
+  //           />
+  //         </div>
+  //       );
+  //     }
+  //     return <p>{paragraph.children}</p>;
+  //   },
+  // };
+
+  const renderers = {
+    image: (image) => {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          height={600}
+          width={300}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={renderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
