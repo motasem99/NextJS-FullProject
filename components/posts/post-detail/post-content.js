2421232b2c0081a7e6c811b523d3f40d7a99556c
mutasem <motasemimad10@gmail.com>
2021-08-29 13:12:46 +0300

    rendering code snippets from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,51 +1,49 @@
-import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
-import PostHeader from './post-header';
+import Image from 'next/image';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
+
 import classes from './post-content.module.css';
+import PostHeader from './post-header';
 
-function PostContent(props) {
-  const { post } = props;
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
+function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
-  // const customRenderers = {
-  //   image(image) {
-  //     return (
-  //       <Image
-  //         src={`/images/posts/${post.slug}/${image.src}`}
-  //         alt={image.alt}
-  //         width={600}
-  //         height={300}
-  //       />
-  //     );
-  //   },
-  //   paragraph(paragraph) {
-  //     const { node } = paragraph;
-  //     if (node.children[0].type === 'image') {
-  //       const image = node.children[0];
-  //       console.log(image);
-  //       return (
-  //         <div className={classes.image}>
-  //           <Image
-  //             src={`/images/posts/${post.slug}/${image.url}`}
-  //             alt={image.alt}
-  //             width={600}
-  //             height={300}
-  //           />
-  //         </div>
-  //       );
-  //     }
-  //     return <p>{paragraph.children}</p>;
-  //   },
-  // };
 
-  const renderers = {
-    image: (image) => {
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
       return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          height={600}
-          width={300}
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
         />
       );
     },
@@ -54,7 +52,7 @@ function PostContent(props) {
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown renderers={renderers}>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
